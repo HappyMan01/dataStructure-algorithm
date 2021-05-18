4851e50acd576a933b2c0d0a1efe58c05ce7515c
happyman <lixiang_zhuizhu@163.com>
2021-05-18 15:06:03 +0800

    doubleLinklist implement for insertAfter method

--- LinkList/DoubleLinkList.java
+++ LinkList/DoubleLinkList.java
@@ -47,7 +47,8 @@ public class DoubleLinkList {
    public  void addToHead(int value) {
        DoubleNode newNode = new DoubleNode(value);
       if(head != null) {
-         newNode.setNextNode(head.getNextNode());
+         newNode.setNextNode(head);
+         head.setPreNode(newNode);
       }
       this.head = newNode;
       this.size++;
@@ -67,17 +68,56 @@ public class DoubleLinkList {
       }
        this.size++;
    }
+   public void insertAfter(int value,int targetValue) {
+       DoubleNode newDoubleNode = new DoubleNode(value);
+       DoubleNode p = head;
+       boolean b = p.getNextNode() != null;
+       if(b) {
+           while (true) {
+               if(p.getNextNode().getValue() == targetValue ) {
+                   // 首先设置newNode
+                   // 然后设置other node
+                   newDoubleNode.setPreNode(p.getNextNode());
+                   newDoubleNode.setNextNode(p.getNextNode().getNextNode());
+                   p.getNextNode().setNextNode(newDoubleNode);
+                   p.getNextNode().getNextNode().setPreNode(newDoubleNode);
+                   break;
+               }
+               p = p.getNextNode();
+           }
+       }else{
+           head.setNextNode(newDoubleNode);
+           newDoubleNode.setPreNode(head);
+       }
+
+
+   }
 
    public void printAll(DoubleLinkList doubleLinkList) {
-       if(this.head == null) {
+       if(doubleLinkList.head == null) {
            System.out.println("doubleLinkList is empty");
        }else {
-          DoubleNode temp = this.head;
-          while (temp.getNextNode() != null) {
-              System.out.printf("doubleLinkList is ");
+          DoubleNode temp = doubleLinkList.head;
+          while (temp != null) {
+              System.out.printf("%d<->",temp.getValue());
+              temp = temp.getNextNode();
           }
        }
+       System.out.println();
    }
+
+    public static void main(String[] args) {
+        DoubleLinkList doubleLinkList = new DoubleLinkList();
+        doubleLinkList.printAll(doubleLinkList);
+        doubleLinkList.addToHead(4);
+        doubleLinkList.printAll(doubleLinkList);
+        doubleLinkList.addToHead(5);
+        doubleLinkList.printAll(doubleLinkList);
+        doubleLinkList.addToTrail(3);
+        doubleLinkList.printAll(doubleLinkList);
+        doubleLinkList.insertAfter(6,4);
+        doubleLinkList.printAll(doubleLinkList);
+    }
 
 
     public DoubleNode getHead() {
